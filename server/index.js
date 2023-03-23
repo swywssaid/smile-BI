2eb248ba5f46d1c3adcb7b33944e51a1b52d6e2f
ssaid <94416699+swywssaid@users.noreply.github.com>
2023-03-24 00:49:49 +0900

    [#14] refactor: 서버 라우트명, API URL명 변경
    - 발급페이지
    - 조회페이지
    - api url
    
    closes #14

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const config = require('./config/index.js');
 const issuance = require('./routes/issuance.js');
-const users = require('./routes/users.js');
+const history = require('./routes/history.js');
 
 const app = express();
 // JSON 형식의 요청 본문 파싱
@@ -10,8 +10,8 @@ app.use(express.json());
 // 폼 데이터 형식의 요청 본문 파싱
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/coupon', issuance);
-app.use('/api/user', users);
+app.use('/api/coupon/issuance', issuance);
+app.use('/api/coupon/history', history);
 
 app.listen(config.port, () => {
   console.log(`listening on port ${config.port}`);
