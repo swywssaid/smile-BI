e776a7acc97ea35b32fec900dfa385274d8b225f
ssaid <94416699+swywssaid@users.noreply.github.com>
2023-03-24 07:03:14 +0900

    [#15] feat: 이름, 번호 검색창 구현

--- client/src/pages/HistoryPage.jsx
+++ client/src/pages/HistoryPage.jsx
@@ -1,5 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Form from '../components/common/Form';
 
 export default function HistoryPage() {
-  return <div>HistoryPage</div>;
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+
+  const handleChangeName = (e) => {
+    setName(e.target.value);
+  };
+  const handleChangePhone = (e) => {
+    setPhone(e.target.value);
+  };
+
+  const nameProps = {
+    type: 'text',
+    value: name,
+    handleChange: handleChangeName,
+    placeholder: '이름을 입력하세요!',
+  };
+
+  const phoneProps = {
+    type: 'text',
+    value: phone,
+    handleChange: handleChangePhone,
+    placeholder: '휴대폰 번호를 입력하세요!',
+  };
+
+  return (
+    <div>
+      <Form {...nameProps} />
+      <Form {...phoneProps} />
+    </div>
+  );
 }
