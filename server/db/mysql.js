6a351f7c61180276bff3192954143c9f111ad717
ssaid <94416699+swywssaid@users.noreply.github.com>
2023-03-23 14:42:42 +0900

    [#10,#12] fix: db 정보 비동기 처리
    - mysql2/promise로 변경
    - pool 사용
    - 등록이 성공했을 때, 쿠폰 번호 응답
    - 등록이 실패했을 때, 이미 등록되어 있는 유저 메시지 응답
    - 서버 에러
    
    closes #10, #12

--- server/db/mysql.js
+++ server/db/mysql.js
@@ -1,12 +1,105 @@
 const config = require('../config/index.js');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
+const voucherCodes = require('voucher-code-generator');
 
-// MySQL 데이터베이스 연결 설정
-const mysqlDB = mysql.createConnection({
+// MySQL 데이터베이스 pool 설정
+const pool = mysql.createPool({
   host: config.mysql.host,
   user: config.mysql.user,
   password: config.mysql.password,
   database: config.mysql.database,
 });
 
-module.exports = mysqlDB;
+// 데이터베이스에서 해당 핸드폰 번호가 있는지 조회
+const queryPhone = async (res, phone) => {
+  try {
+    const connection = await pool.getConnection(async (conn) => conn);
+    try {
+      /* Step 3. */
+      const [rows] = await connection.execute(
+        'SELECT * FROM coupon_total WHERE phone_number = ?',
+        [phone]
+      );
+      connection.release();
+      if (rows.length === 0) {
+        return await getUniqueCoupon(res);
+      }
+
+      if (rows.length > 0) {
+        res
+          .status(400)
+          .json({ success: false, message: '이미 발급받은 핸드폰 번호입니다' });
+        return false;
+      }
+    } catch (err) {
+      connection.release();
+      console.log(`DB queryPhone err: ${err} `);
+      return false;
+    }
+  } catch (err) {
+    console.log(`DB queryPhone err: ${err} `);
+    return false;
+  }
+};
+
+// 유니크 쿠폰 번호 생성 함수
+const getUniqueCoupon = async () => {
+  try {
+    const connection = await pool.getConnection(async (conn) => conn);
+    let isDuplicated = true;
+    let couponCode;
+    while (isDuplicated) {
+      [couponCode] = await voucherCodes.generate({
+        length: 12,
+        count: 1,
+        charset: '0123456789',
+        pattern: '####-####-####',
+      });
+
+      try {
+        const [rows] = await connection.execute(
+          'SELECT * FROM coupon_total WHERE coupon_code = ?',
+
+          [couponCode]
+        );
+        connection.release();
+
+        if (rows.length === 0) isDuplicated = false;
+      } catch (err) {
+        connection.release();
+        console.log(`DB getUniqueCoupon err: ${err} `);
+        return false;
+      }
+    }
+
+    return couponCode;
+  } catch (err) {
+    console.log(`DB getUniqueCoupon err: ${err} `);
+    return false;
+  }
+};
+
+// 데이터베이스에 저장
+const insertUserInfo = async (res, phone, name, couponCode) => {
+  try {
+    const connection = await pool.getConnection(async (conn) => conn);
+
+    try {
+      await connection.execute(
+        'INSERT INTO coupon_total (phone_number, name, coupon_code, created_at) VALUES (?, ?, ?, default)',
+        [phone, name, couponCode]
+      );
+      connection.release();
+
+      return res.status(200).json({ success: true, data: couponCode });
+    } catch (err) {
+      connection.release();
+      console.log(`DB insertUserInfo err: ${err} `);
+      return res.status(500).send({ message: `DB insertUserInfo err: ${err}` });
+    }
+  } catch (err) {
+    return res.status(500).send({ message: `DB insertUserInfo err: ${err}` });
+  }
+};
+
+module.exports = { queryPhone: queryPhone, insertUserInfo: insertUserInfo };
--- server/routes/issuance.js
+++ server/routes/issuance.js
@@ -1,73 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const mysqlDB = require('../db/mysql');
-const voucherCodes = require('voucher-code-generator');
-
-async function getUniqueCoupon(res) {
-  let isDuplicated = true;
-  let couponCode;
-  while (isDuplicated) {
-    [couponCode] = await voucherCodes.generate({
-      length: 12,
-      count: 1,
-      charset: '0123456789',
-      pattern: '####-####-####',
-    });
-
-    try {
-      const { _rows } = await mysqlDB.execute(
-        'SELECT * FROM coupon_total WHERE coupon_code = ?',
-
-        [couponCode]
-      );
-
-      if (_rows.length === 0) isDuplicated = false;
-    } catch (err) {
-      res.status(500).json({ message: `쿠폰 번호 조회 서버 오류: ${err}` });
-    }
-  }
-
-  return couponCode;
-}
+const { queryPhone, insertUserInfo } = require('../db/mysql');
 
 // 쿠폰 발급 api
 router.post('/', async (req, res) => {
   const { name, phone } = req.body;
-  let couponCode;
-
-  // 데이터베이스에서 해당 번호가 있는지 조회
-  try {
-    const { _rows } = await mysqlDB.execute(
-      'SELECT * FROM coupon_total WHERE phone_number = ?',
-      [phone]
-    );
-
-    if (_rows.length === 0) {
-      couponCode = await getUniqueCoupon(res);
-    } else {
-      return res
-        .status(400)
-        .json({ message: '이미 발급받은 핸드폰 번호입니다' });
-    }
-  } catch (err) {
-    return res
-      .status(500)
-      .json({ message: `쿠폰 번호 조회 서버 오류: ${err}` });
-  }
-
-  // 데이터베이스에 저장
-  try {
-    await mysqlDB.execute(
-      'INSERT INTO coupon_total (phone_number, name, coupon_code, created_at) VALUES (?, ?, ?, default)',
-      [phone, name, couponCode]
-    );
-
-    return res.status(200).json({ data: couponCode });
-  } catch (err) {
-    return res
-      .status(500)
-      .json({ message: `쿠폰 번호 저장 서버 오류: ${err}` });
-  }
+  const couponCode = await queryPhone(res, phone);
+  if (!couponCode) return;
+  await insertUserInfo(res, phone, name, couponCode);
 });
 
 module.exports = router;
